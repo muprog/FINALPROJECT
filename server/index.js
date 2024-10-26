562977b56c8055d0ef879b01f99d1780608226de
muprog <128657551+muprog@users.noreply.github.com>
2024-10-26 15:00:18 +0300

    second commit

--- server/index.js
+++ server/index.js
@@ -1,6 +1,6 @@
 const express=require('express');
 const app=express();
-const port=8000;
+const port=process.env.PORT||8000;
 const cookieParser=require('cookie-parser')
 const dotenv=require('dotenv').config();
 const mongoose=require('mongoose');
